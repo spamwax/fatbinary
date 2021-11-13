aa2be15c7d88d63a8db083cfffebf245cd63ffd5
Hamid Ghadyani <hamid@hamid.cc>
2021-11-13 05:41:35 -0500

    Testing ssh

--- .circleci/script.sh
+++ .circleci/script.sh
@@ -64,5 +64,6 @@ cargo build --target x86_64-apple-darwin
 
 SDKROOT=$(xcrun -sdk macosx11.0 --show-sdk-path) MACOSX_DEPLOYMENT_TARGET=$(xcrun -sdk macosx11.0 --show-sdk-platform-version) cargo build --target=aarch64-apple-darwin
 
+
 lipo -create -output universal target/aarch64-apple-darwin/debug/fatbinary target/x86_64-apple-darwin/debug/fatbinary
 
