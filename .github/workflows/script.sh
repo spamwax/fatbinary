3e060f6b6d713bc3b49e3d0f4b281517c8084404
Hamid Ghadyani <hamid@hamid.cc>
2021-11-14 05:41:20 -0500

    RC 2

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -2,7 +2,7 @@
 
 # ls -la ./target/aarch64-apple-darwin/debug
 # ls -la ./target/x86_64-apple-darwin/debug
-# set -ex
+set -ex
 
 echo "Release: =$RELEASE="
 echo "RELEASE_COMMIT: =$RELEASE_COMMIT="
@@ -12,11 +12,15 @@ echo "FOOK: =$FOOK="
 echo "GLOBAL: =$GLOBAL="
 
 if [[ "$GLOBAL" = "true" ]]; then
+    ls -lh ./target/aarch64-apple-darwin/release/fatbinary
+    ls -lh ./target/x86_64-apple-darwin/release/fatbinary
     lipo -create -output fatbinary target/aarch64-apple-darwin/release/fatbinary target/x86_64-apple-darwin/release/fatbinary
     file ./fatbinary
     chmod u+x ./fatbinary
     zip fatbinary.zip fatbinary
 else
+    ls -lh ./target/aarch64-apple-darwin/debug/fatbinary
+    ls -lh ./target/x86_64-apple-darwin/debug/fatbinary
     lipo -create -output fatbinary target/aarch64-apple-darwin/debug/fatbinary target/x86_64-apple-darwin/debug/fatbinary
     ./fatbinary
 fi
