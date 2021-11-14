d8720eb9d935d417bd967d186b6329555f6c055f
Hamid Ghadyani <hamid@hamid.cc>
2021-11-14 01:51:13 -0500

    Try to run the executable.

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-ls -la ./target/aarch64-apple-darwin/debug
-ls -la ./target/x86_64-apple-darwin/debug
+# ls -la ./target/aarch64-apple-darwin/debug
+# ls -la ./target/x86_64-apple-darwin/debug
+set -ex
 lipo -create -output fatbinary target/aarch64-apple-darwin/debug/fatbinary target/x86_64-apple-darwin/debug/fatbinary
-ls -la
+file ./fatbinary
+
+./fatbinary
