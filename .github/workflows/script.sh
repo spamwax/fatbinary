dc768b22de97a1d64d8d94cdb8606115249c989e
Hamid Ghadyani <hamid@hamid.cc>
2021-11-14 03:52:32 -0500

    Try to detect release vs test.

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -3,6 +3,9 @@
 # ls -la ./target/aarch64-apple-darwin/debug
 # ls -la ./target/x86_64-apple-darwin/debug
 set -ex
+
+echo "$RELEASE"
+
 lipo -create -output fatbinary target/aarch64-apple-darwin/debug/fatbinary target/x86_64-apple-darwin/debug/fatbinary
 file ./fatbinary
 
